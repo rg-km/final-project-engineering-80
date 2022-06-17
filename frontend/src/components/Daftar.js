9875db0c59231daa737679a0a3c7342ff4406231
ilhamr <hfzrzky489@gmail.com>
2022-06-17 21:11:38 +0700

    add daftar

--- frontend/src/components/Daftar.js
+++ frontend/src/components/Daftar.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Daftar = () => {
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const changeUsername = (e) => {
+        const value = e.target.value
+        setUsername(value)
+    }
+
+    const changeEmail = (e) => {
+        const value = e.target.value
+        setEmail(value)
+    }
+
+    const changePassword = (e) => {
+        const value = e.target.value
+        setPassword(value)
+    }
+
     return (
         <div style={{marginTop: "170px" }}>
             <div className="container">
@@ -10,17 +29,17 @@ const Daftar = () => {
                             <div className="card-body">
                                 <div className="form-group">
                                     <label>Username</label>
-                                    <input type="text" placeholder="Username" className="form-control" />
+                                    <input type="text" placeholder="Username" className="form-control" value={username} onChange={changeUsername} />
                                 </div>
 
                                 <div className="form-group">
                                     <label>Email</label>
-                                    <input type="text" placeholder="Email" className="form-control" />
+                                    <input type="text" placeholder="Email" className="form-control" value={email} onChange={changeEmail} />
                                 </div>
 
                                 <div className="form-group">
                                     <label>Password</label>
-                                    <input type="password" placeholder="Password" className="form-control" />
+                                    <input type="password" placeholder="Password" className="form-control" value={password} onChange={changePassword} />
                                 </div>
                             <button className="btn btn-primary">Daftar</button>
                             </div>
